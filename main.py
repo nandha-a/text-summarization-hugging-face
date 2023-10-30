813a3065d7b8956842a80d58886289396e08bdf7
Nandha_A <nandhakumara56@gmail.com>
2023-10-30 13:16:18 +0530

    Data Ingestion

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from textSummarizer.logging import logger
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom log")
+
+STAGE_NAME = " Data Ingestion"
+
+try:
+    logger.info(f">>>>>>>> statge {STAGE_NAME} started <<<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
