685493dbfb9d59e848ad2fd204d9af20275a69a8
Nandha_A <nandhakumara56@gmail.com>
2023-10-30 20:17:41 +0530

    data validation pipeline

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
-STAGE_NAME = " Data Ingestion"
+STAGE_NAME = "Data Ingestion"
 
 try:
     logger.info(f">>>>>>>> statge {STAGE_NAME} started <<<<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation"
+
+try:
+    logger.info(f">>>>>>>> statge {STAGE_NAME} started <<<<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
 except Exception as e:
---
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -0,0 +1,26 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.data_validation import DataValidation
+from textSummarizer.logging import logger
+
+STAGE_NAME = " Data Validation"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_files_exist()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>>> statge {STAGE_NAME} started <<<<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
